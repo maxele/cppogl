364bce24ed9f585e15ce11ff71e2492c8e32389c
maxelee <maxmaxeleee@gmail.com>
2025-02-13 19:00:49 +0100

    Fixed formatting

--- BufferObject.cpp
+++ BufferObject.cpp
@@ -2,25 +2,24 @@ class BufferObject
 {
 public:
 	GLenum _type;
+	GLuint _id;
 	BufferObject(GLenum type);
 	~BufferObject();
 	void loadData(int count, void **data, GLenum usage);
 	void bind();
 	void draw(GLsizei count);
-private:
-	GLuint id;
 };
 BufferObject::BufferObject(GLenum type) {
 	_type = type;
-	glGenBuffers(1, &id);
+	glGenBuffers(1, &_id);
 }
 BufferObject::~BufferObject() {
-	glDeleteBuffers(1, &id);
+	glDeleteBuffers(1, &_id);
 }
 void BufferObject::loadData(int count, void **data, GLenum usage) {
 	bind();
 	glBufferData(_type, count, data, usage);
 }
 void BufferObject::bind() {
-	glBindBuffer(_type, id);
+	glBindBuffer(_type, _id);
 }
